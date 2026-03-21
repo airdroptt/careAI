32c0dea62c19936f5df7c4f5f01935ad3118f54a
airdroptt <airdroptt66@gmail.com>
2026-03-21 09:45:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-export const API_BASE = window.location.hostname === "localhost"
-  ? "http://localhost:3000"
-  : "https://web-admin-ck6m.onrender.com";
+export const API_BASE = "https://web-admin-ck6m.onrender.com";
